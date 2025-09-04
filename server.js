1daf5c58f28882541b24b657e47a4e2ef799450f
Rocky-807 <venkateshsangam807@gmail.com>
2025-09-04 16:36:04 +0530

    Add all project files

---
+++ server.js
@@ -0,0 +1,15 @@
+// src/index.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Define a route for the root URL "/"
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
